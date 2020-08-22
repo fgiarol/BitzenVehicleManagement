c9dca262b49c8ca4b81d99b068554d30a7aac75e
Fernando Giarola <fernandogiarola97@gmail.com>
2020-08-22 20:22:36 -0300

    Initial migration and add services in ConfigureServices

--- BitzenVehicleManagementAPI/Startup.cs
+++ BitzenVehicleManagementAPI/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BitzenVehicleManagementAPI.Data;
+using BitzenVehicleManagementAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +29,14 @@ namespace BitzenVehicleManagementAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BitzenApplicationContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("default"), builder =>
+                        builder.MigrationsAssembly("BitzenVehicleManagementAPI")));
+
+            services.AddIdentity<User, IdentityRole<long>>()
+                .AddEntityFrameworkStores<BitzenApplicationContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllers();
         }
 
