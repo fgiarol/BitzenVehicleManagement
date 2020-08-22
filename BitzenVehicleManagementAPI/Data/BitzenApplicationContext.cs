81c9eac66934c525168c61e278e5ef5bd82acc69
Fernando Giarola <fernandogiarola97@gmail.com>
2020-08-22 20:08:35 -0300

    Create DataContext and Dbset to the entities

---
+++ BitzenVehicleManagementAPI/Data/BitzenApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using BitzenVehicleManagementAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitzenVehicleManagementAPI.Data
+{
+    public class BitzenApplicationContext : IdentityDbContext<User, IdentityRole<long>, long>
+    {
+        public BitzenApplicationContext(DbContextOptions<BitzenApplicationContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Fueling> Fuelings { get; set; }
+    }
+}
